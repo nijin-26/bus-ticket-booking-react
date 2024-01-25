1ffa8283753fe22546a838d568a8f7dd25004638
amaljoeqb <amal.joe@qburst.com>
2024-01-25 13:50:47 +0530

    add null check to root element

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById('root');
+if (rootElement) {
+ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
+} else {
+    throw new Error('Root element not found');
+}
