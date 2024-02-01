01eef6e90ce97f2d66bdae1b445a52e9c37d141f
amaljoeqb <amal.joe@qburst.com>
2024-02-01 12:42:50 +0530

    [update]: user card component

--- src/pages/UserListing/components/UserCard/UserCard.tsx
+++ src/pages/UserListing/components/UserCard/UserCard.tsx
@@ -3,7 +3,9 @@ import { UserCardWrapper } from './UserCard.styled';
 export const UserCard = () => {
     return (
         <UserCardWrapper>
-            <h1>UserCard</h1>
+            <div className="fullName">John Doe</div>
+            <div className="email">johndoe@gmail.com</div>
+            <div className="phone">+1 123 456 7890</div>
         </UserCardWrapper>
     );
 };
