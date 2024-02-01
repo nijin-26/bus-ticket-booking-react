25ce073d56ca65119eb4c12dd5c636d86305a4d5
Akshay Krishna <akshay.krishna@qburst.com>
2024-02-01 17:23:56 +0530

    [feat]: Intial commit of TripListing card UI

---
+++ src/components/ui/TripCard/TripCardAccordion.tsx
@@ -0,0 +1,88 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import longArrow from '../../../assets/Long_arrow.svg';
+import acIcon from '../../../assets/AcIcon.svg';
+import nonAcIcon from '../../../assets/NonAcIcon.svg';
+import seatIcon from '../../../assets/SeatIcon.svg';
+import sleeperIcon from '../../../assets/SleeperIcon.svg';
+import Stack from '@mui/material/Stack';
+
+import { TripCardStyled } from './TripCard.styled';
+const TripCardAccordion = () => {
+    return (
+        <TripCardStyled className="accordion">
+            <Accordion className="summary">
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    <Stack direction={'row'} spacing={12} className="details">
+                        <Stack className="trip-card-icons">
+                            <img src={acIcon} alt="Ac Icon" />
+                            <img src={sleeperIcon} alt="Sleeper Icon" />
+                        </Stack>
+                        <Stack direction={'row'} spacing={4}>
+                            <Stack className="date-time">
+                                <p>5.00 AM</p> <p className="date">20 Jan</p>
+                            </Stack>
+                            <img src={longArrow} alt="long-arrow" />
+                            <Stack className="date-time">
+                                <p>18.40 PM</p> <p className="date">23 Jan</p>
+                            </Stack>
+                        </Stack>
+                        <p>3 days, 13 hours</p>
+                        <p>36 seats available</p>
+                        <p>Rs. 1300/-</p>
+                    </Stack>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse malesuada lacus ex, sit amet blandit leo
+                        lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion className="summary">
+                <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    <Stack direction={'row'} spacing={12} className="details">
+                        <Stack className="trip-card-icons">
+                            <img src={nonAcIcon} alt="Non Ac Icon" />
+                            <img src={seatIcon} alt="Seat Icon" />
+                        </Stack>
+                        <Stack direction={'row'} spacing={4}>
+                            <Stack className="date-time">
+                                <p>5.00 AM</p> <p className="date">20 Jan</p>
+                            </Stack>
+                            <img src={longArrow} alt="long-arrow" />
+                            <Stack className="date-time">
+                                <p>18.40 PM</p> <p className="date">23 Jan</p>
+                            </Stack>
+                        </Stack>
+                        <p>3 days, 13 hours</p>
+                        <p>36 seats available</p>
+                        <p>Rs. 1300/-</p>
+                    </Stack>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Suspendisse malesuada lacus ex, sit amet blandit leo
+                        lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </TripCardStyled>
+    );
+};
+
+export default TripCardAccordion;
---
+++ src/components/ui/TripCard/TripCardStyled.ts
@@ -0,0 +1,7 @@
+import { styled } from '@mui/material';
+
+export const TripCardStyled = styled('section')`
+display: flex;
+flex-direction: column;
+`
+
