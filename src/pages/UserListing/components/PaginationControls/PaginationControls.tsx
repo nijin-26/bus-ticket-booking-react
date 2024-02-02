392724b325675ab7e619d0a82b11547e5990cc96
amaljoeqb <amal.joe@qburst.com>
2024-02-02 09:52:05 +0530

    [remove]: pagination controls

--- src/pages/UserListing/components/PaginationControls/PaginationControls.tsx
+++
@@ -1,9 +0,0 @@
-import { PaginationControlsWrapper } from './PaginationControls.styled';
-
-export const PaginationControls = () => {
-    return (
-        <PaginationControlsWrapper>
-            <h1>PaginationControls</h1>
-        </PaginationControlsWrapper>
-    );
-};
