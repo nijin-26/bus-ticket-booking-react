755369094592d0688846239c660f37ad35ebf286
Abhijith Biju <abhijith.biju@qburst.com>
2024-02-05 18:31:24 +0530

    feat(tripBookingForm): add formik wrap

--- src/pages/TripBookingPage/TripBookingPage.tsx
+++ src/pages/TripBookingPage/TripBookingPage.tsx
@@ -1,71 +1,162 @@
-import { useState } from 'react';
-import {
-    Box,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogTitle,
-    Grid,
-    Typography,
-} from '@mui/material';
-import PassengerDetails from './components/PassengerDetails';
+import { Formik, Form, FieldArray, Field } from 'formik';
+// import PassengerDetails from './components/PassengerDetails';
 import TicketTotalFare from './components/TicketTotalFare';
+import FormControl from '@mui/material/FormControl';
+import { Box, Button, Grid, MenuItem, Paper, Typography } from '@mui/material';
+import getValidationSchema from './validation';
+import { Select, TextField } from 'formik-mui';
 
 export const TripBookingPage = () => {
-    const [open, setOpen] = useState(false);
+    const genders = [
+        {
+            value: 'male',
+            label: 'Male',
+        },
+        {
+            value: 'female',
+            label: 'Female',
+        },
+        {
+            value: 'other',
+            label: 'Other',
+        },
+    ];
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
+    const generateInitialValues = (selectedSeatCount: number) =>
+        Array.from({ length: selectedSeatCount }, () => ({
+            fullName: '',
+            age: '',
+            gender: '',
+        }));
 
     return (
-        <>
-            <Box component="form">
-                <Typography component="h3" variant="h5" mb={2} mt={4}>
-                    Passenger Details
-                </Typography>
-                <PassengerDetails passengerNumber={1} seatNumber={12} />
-                <PassengerDetails passengerNumber={2} seatNumber={13} />
-                <Grid container>
-                    <Grid item xs={12} sm={9}>
-                        <TicketTotalFare
-                            ticketFare={1200}
-                            noOfSeatsBooked={4}
-                        />
+        <Formik
+            initialValues={{ passengers: generateInitialValues(1) }}
+            validationSchema={getValidationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    setSubmitting(false);
+                    alert(JSON.stringify(values, null, 2));
+                }, 500);
+            }}
+        >
+            {({ values }) => (
+                <Form autoComplete="off" noValidate>
+                    <Typography component="h3" variant="h5" mb={2} mt={4}>
+                        Passenger Details
+                    </Typography>
+                    <FieldArray name="passengers">
+                        {() =>
+                            values.passengers.map((_, index) => (
+                                <Box
+                                    key={index}
+                                    component={Paper}
+                                    py="10px"
+                                    px="20px"
+                                    mb="30px"
+                                    elevation={4}
+                                >
+                                    <Typography
+                                        component="h4"
+                                        variant="h6"
+                                        mb={2}
+                                    >
+                                        {`Passenger 1 | seat 12`}
+                                    </Typography>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12} md={6}>
+                                            {/* <TextField
+                                                fullWidth
+                                                required
+                                                id={'fullname'}
+                                                name={`passengers.${index}.fullName`}
+                                                label="Full Name"
+                                                size="small"
+                                            /> */}
+                                            <Field
+                                                fullWidth
+                                                component={TextField}
+                                                name={`passengers.${index}.fullName`}
+                                                label="Full Name"
+                                                size="small"
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <Field
+                                                fullWidth
+                                                component={TextField}
+                                                id={'age'}
+                                                name={`passengers.${index}.age`}
+                                                label="Age"
+                                                size="small"
+                                                helperText=" "
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <Field
+                                                fullWidth
+                                                component={Select}
+                                                name={`passengers.${index}.gender`}
+                                                label="Gender"
+                                                labelId="gender-simple"
+                                                size="small"
+                                                helperText=" "
+                                                value=""
+                                            >
+                                                {genders.map((option) => (
+                                                    <MenuItem
+                                                        key={option.value}
+                                                        value={option.value}
+                                                    >
+                                                        {option.label}
+                                                    </MenuItem>
+                                                ))}
+                                            </Field>
+                                            {/* <Field
+                                                fullWidth
+                                                component={Select}
+                                                formControl={{
+                                                    sx: FormControl,
+                                                }}
+                                                id={'gender'}
+                                                name={`passengers.${index}.gender`}
+                                                label="Gender"
+                                                labelId="gender-select"
+                                                size="small"
+                                                helperText=" "
+                                                value=""
+                                            >
+                                                {genders.map((option) => (
+                                                    <MenuItem
+                                                        key={option.value}
+                                                        value={option.value}
+                                                    >
+                                                        {option.label}
+                                                    </MenuItem>
+                                                ))}
+                                            </Field> */}
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            ))
+                        }
+                    </FieldArray>
+
+                    <Grid container>
+                        <Grid item xs={12} sm={9}>
+                            <TicketTotalFare
+                                ticketFare={1200}
+                                noOfSeatsBooked={4}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={3} ml="auto">
+                            <Button variant="contained" type="submit" fullWidth>
+                                Checkout
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={3} ml="auto">
-                        <Button
-                            variant="contained"
-                            onClick={handleClickOpen}
-                            fullWidth
-                        >
-                            Checkout
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Box>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {'Proceed with the booking?'}
-                </DialogTitle>
-                <DialogActions>
-                    <Button variant="outlined" onClick={handleClose}>
-                        Cancel
-                    </Button>
-                    <Button variant="contained" onClick={handleClose} autoFocus>
-                        Yes
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
+                </Form>
+            )}
+        </Formik>
     );
 };
--- src/pages/TripBookingPage/components/PassengerDetails.tsx
+++ src/pages/TripBookingPage/components/PassengerDetails.tsx
@@ -41,20 +41,18 @@ const PassengerDetails = ({
                     <TextField
                         fullWidth
                         required
-                        id={`${passengerNumber}-fullName`}
-                        name={`${passengerNumber}-fullName`}
+                        id={'fullname'}
+                        name={'fullName'}
                         label="Full Name"
                         size="small"
-                        // error
-                        // helperText="Full name is a required field"
                     />
                 </Grid>
                 <Grid item xs={12} sm={6} md={3}>
                     <TextField
                         fullWidth
                         required
-                        id={`${passengerNumber}-age`}
-                        name={`${passengerNumber}-age`}
+                        id={'age'}
+                        name={'age'}
                         label="Age"
                         size="small"
                         helperText=" "
@@ -64,8 +62,8 @@ const PassengerDetails = ({
                     <TextField
                         fullWidth
                         select
-                        id={`${passengerNumber}-gender`}
-                        name={`${passengerNumber}-gender`}
+                        id={'gender'}
+                        name={'gender'}
                         label="Gender"
                         size="small"
                         helperText=" "
@@ -82,5 +80,76 @@ const PassengerDetails = ({
         </Box>
     );
 };
+
+// const PassengerDetails = ({
+//     passengerNumber,
+//     seatNumber,
+// }: IPassengerDetails) => {
+//     const genders = [
+//         {
+//             value: 'male',
+//             label: 'Male',
+//         },
+//         {
+//             value: 'female',
+//             label: 'Female',
+//         },
+//         {
+//             value: 'other',
+//             label: 'Other',
+//         },
+//     ];
+
+//     return (
+//         <Box component={Paper} py="10px" px="20px" mb="30px" elevation={4}>
+//             <Typography component="h4" variant="h6" mb={2}>
+//                 {`Passenger ${passengerNumber} | seat ${seatNumber}`}
+//             </Typography>
+//             <Grid container spacing={2}>
+//                 <Grid item xs={12} md={6}>
+//                     <TextField
+//                         fullWidth
+//                         required
+//                         id={`${passengerNumber}-fullName`}
+//                         name={`${passengerNumber}-fullName`}
+//                         label="Full Name"
+//                         size="small"
+//                         // error
+//                         // helperText="Full name is a required field"
+//                     />
+//                 </Grid>
+//                 <Grid item xs={12} sm={6} md={3}>
+//                     <TextField
+//                         fullWidth
+//                         required
+//                         id={`${passengerNumber}-age`}
+//                         name={`${passengerNumber}-age`}
+//                         label="Age"
+//                         size="small"
+//                         helperText=" "
+//                     />
+//                 </Grid>
+//                 <Grid item xs={12} sm={6} md={3}>
+//                     <TextField
+//                         fullWidth
+//                         select
+//                         id={`${passengerNumber}-gender`}
+//                         name={`${passengerNumber}-gender`}
+//                         label="Gender"
+//                         size="small"
+//                         helperText=" "
+//                         value=""
+//                     >
+//                         {genders.map((option) => (
+//                             <MenuItem key={option.value} value={option.value}>
+//                                 {option.label}
+//                             </MenuItem>
+//                         ))}
+//                     </TextField>
+//                 </Grid>
+//             </Grid>
+//         </Box>
+//     );
+// };
 
 export default PassengerDetails;
