eae2dd5dc9ac21816d30bca5a4cb10f85725af51
Your Name <you@example.com>
2024-02-07 12:31:51 +0530

    feat: illustrations for light and dark theme

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,23 @@
+import { useTheme } from '@mui/material';
 import ActionBarTab from '../../components/actionBar/actionBarTab/ActionBarTab';
 import { LandingPageWrapper } from './LandingPage.styled';
-import LandingPageIllustration from './LandingPageIllustration';
+import LandingPageIllustration from './LandingPageIllustrationLight';
+import LandingPageIllustrationDark from './LandingPageIllustrationDark copy';
+import LandingPageIllustrationLight from './LandingPageIllustrationLight';
 
 export const LandingPage = () => {
+    const theme = useTheme();
+    const currentTheme = theme.palette.mode;
+    console.log(currentTheme);
+
     return (
         <LandingPageWrapper>
-            <LandingPageIllustration />
+            {currentTheme === 'dark' ? (
+                <LandingPageIllustrationDark />
+            ) : (
+                <LandingPageIllustrationLight />
+            )}
+
             <h1>Bus with us.</h1>
             <ActionBarTab />
         </LandingPageWrapper>
