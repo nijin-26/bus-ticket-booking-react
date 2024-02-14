6f62d2a261f415cbccab8bc3ee6e4f0517a74ca5
Amal Joe R S <amal.joe@qburst.com>
2024-02-14 15:07:37 +0530

    add: getTrip

--- src/api/endpoints/trips.api.ts
+++ src/api/endpoints/trips.api.ts
@@ -1,5 +1,12 @@
 import { API, apiRoutes } from '..';
-import { ITrip, ITripsQueryRequest, ITripsQueryResponse } from '../types/trip';
+import {
+    ISeatStatus,
+    ITrip,
+    ITripDetailRequest,
+    ITripDetailResponse,
+    ITripsQueryRequest,
+    ITripsQueryResponse,
+} from '../types/trip';
 
 export const getTrips = async (
     params: ITripsQueryRequest
@@ -8,4 +15,18 @@ export const getTrips = async (
         params,
     });
     return response.trips;
+};
+
+export const getTrip = async (
+    id: string
+): Promise<ITripDetailResponse | undefined> => {
+    // TODO: replace with real API endpoint once availabe. Now using trips API with mock data for seats[].
+    const response: ITripsQueryResponse = await API.get(apiRoutes.trip);
+    const trip = response.trips.find((trip) => trip.id == id);
+    if (!trip) return undefined;
+    const tripDetail: ITripDetailResponse = { ...trip, seats: [] };
+    for (let i = 1; i <= trip.totalSeats; i++) {
+        tripDetail.seats.push({ seatNumber: i, status: ISeatStatus.AVAILABLE });
+    }
+    return tripDetail;
 };
