22eca1d71c9c185b794dd25a7655c31c27d38a46
seraGeorge <sera.george@qburst.com>
2024-02-28 16:52:26 +0530

    feat: created custom hook for fetching data for ticket

--- src/components/Ticket/TicketModal.tsx
+++ src/components/Ticket/TicketModal.tsx
@@ -1,13 +1,10 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { Overlay } from '../actionBar/pnrSearch/PnrSearch.styled';
 import { Ticket } from './Ticket';
 import FullScreenLoader from '../FullScreenLoader/FullScreenLoader';
-import { useTranslation } from 'react-i18next';
-import { useLocation, useSearchParams } from 'react-router-dom';
-import { ITicket } from '../../types';
-import { getTicketByPnr } from '../../api/endpoints/ticket.api';
-import { toast } from 'react-toastify';
 import CloseIcon from '@mui/icons-material/Close';
+import { useGetTicketData } from './utils/useGetTicketData';
+import { useSearchParams } from 'react-router-dom';
 
 export const TicketModal = ({
     showTicketStateObject,
@@ -17,43 +14,15 @@ export const TicketModal = ({
         setShowTicket: Dispatch<SetStateAction<boolean>>;
     };
 }) => {
-    const location = useLocation();
     const [searchParams, setSearchParams] = useSearchParams();
     const pnrNumber: string | null = searchParams.get('pnr');
-    const { t } = useTranslation('error');
 
-    const ticketDataFromState = location.state as ITicket | null;
+    const { ticketData, loading } = useGetTicketData(pnrNumber);
 
-    const [ticketData, setTicketData] = useState<ITicket>();
-    const [loading, setLoading] = useState<boolean>(true);
-
-    const errorText = t('unexpected');
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                if (pnrNumber) {
-                    const response = await getTicketByPnr(pnrNumber);
-                    setTicketData(response);
-                }
-            } catch (error) {
-                toast.error(errorText, { toastId: 'Error toast ' });
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (!ticketDataFromState) {
-            void fetchData();
-        } else {
-            setTicketData(ticketDataFromState);
-            setLoading(false);
-        }
-    }, [errorText, pnrNumber, ticketDataFromState]);
-
-    return loading ? (
-        <FullScreenLoader open={loading} />
-    ) : (
+    if (loading) {
+        return <FullScreenLoader open={loading} />;
+    }
+    return (
         ticketData && (
             <Overlay
                 onClick={() => {
--- src/pages/TicketPage/TicketPage.tsx
+++ src/pages/TicketPage/TicketPage.tsx
@@ -1,54 +1,23 @@
-import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { getTicketByPnr } from '../../api/endpoints/ticket.api';
-import { ITicket } from '../../types';
+import {  useNavigate, useParams } from 'react-router-dom';
 import { Ticket } from '../../components';
 import FullScreenLoader from '../../components/FullScreenLoader/FullScreenLoader';
 import { Home } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import { paths } from '../../config';
+import { useGetTicketData } from '../../components/Ticket/utils/useGetTicketData';
 
 export const TicketPage = () => {
-    const location = useLocation();
     const { pnrNumber } = useParams();
-    const { t } = useTranslation(['error', 'errorPage']);
     const navigate = useNavigate();
 
-    const ticketDataFromState = location.state as ITicket | null;
-
-    const [ticketData, setTicketData] = useState<ITicket>();
-    const [loading, setLoading] = useState<boolean>(true);
-
-    const errorText = t('error:unexpected');
+    const { t } = useTranslation('errorPage');
 
     const goHomeHandler = () => {
         navigate(paths.home);
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                if (pnrNumber) {
-                    const response = await getTicketByPnr(pnrNumber);
-                    console.log(response);
-                    setTicketData(response);
-                }
-            } catch (error) {
-                toast.error(errorText);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (!ticketDataFromState) {
-            void fetchData();
-        } else {
-            setTicketData(ticketDataFromState);
-            setLoading(false);
-        }
-    }, [errorText, pnrNumber, ticketDataFromState]);
+    const { ticketData, loading } = useGetTicketData(pnrNumber);
 
     if (loading) {
         return <FullScreenLoader open={loading} />;
@@ -61,9 +30,9 @@ export const TicketPage = () => {
                     variant="contained"
                     onClick={goHomeHandler}
                     startIcon={<Home />}
-                    sx={{marginTop:'10px'}}
+                    sx={{ marginTop: '10px' }}
                 >
-                    {t('errorPage:goHome')}
+                    {t('goHome')}
                 </Button>
                 <Ticket data={ticketData} />
             </>
