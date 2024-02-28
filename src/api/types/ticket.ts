543fb7cee6dda90b425a01844ada9c4612a29749
amaljoeqb <amal.joe@qburst.com>
2024-02-28 10:31:12 +0530

    add: my bookings endpoint

--- src/api/types/ticket.ts
+++ src/api/types/ticket.ts
@@ -50,4 +50,6 @@ export interface IPnrResponse extends ITripExternal {
     }[];
 }
 
-export type IMyBookingsResponse = IPnrResponse[];
+export interface IMyBookingsResponse {
+    bookings: IPnrResponse[];
+}
