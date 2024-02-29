157946065ace236ec6f339e719b7bcff10bbb134
RISHI J R <rishi@qburst.com>
2024-02-29 16:38:00 +0530

    fix: remove commented line and add more checks

--- src/pages/TripBookingPage/TripBookingPage.tsx
+++ src/pages/TripBookingPage/TripBookingPage.tsx
@@ -55,68 +55,70 @@ export const TripBookingPage = () => {
     }, [navigate, state, t]);
 
     return (
-        <>
-            <Stack direction="row" alignItems="center" mt={4} mb={3} gap="2rem">
-                <Typography component="h1" variant="h4">
-                    {state.origin.name}
-                </Typography>
-                <LongArrow width="8rem" height="100%" />
-                <Typography component="h1" variant="h4">
-                    {state.destination.name}
-                </Typography>
-            </Stack>
-            {/* <TripCardAccordion
-                defaultExpanded={true}
-                data={state}
-                mode="view"
-            />
-            <PassengerDetailsForm /> */}
-            <TripCardAccordion
-                defaultExpanded={true}
-                data={state}
-                mode="view"
-            />
-            <PassengerDetailsForm
-                formikRef={formikRef}
-                selectedSeats={selectedSeats}
-                loaderFun={setShowLoaderFun}
-                tripId={state.id}
-            />
+        state.seats.length && (
+            <>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    mt={4}
+                    mb={3}
+                    gap="2rem"
+                >
+                    <Typography component="h1" variant="h4">
+                        {state.origin.name}
+                    </Typography>
+                    <LongArrow width="8rem" height="100%" />
+                    <Typography component="h1" variant="h4">
+                        {state.destination.name}
+                    </Typography>
+                </Stack>
+                <TripCardAccordion
+                    defaultExpanded={true}
+                    data={state}
+                    mode="view"
+                />
+                <PassengerDetailsForm
+                    formikRef={formikRef}
+                    selectedSeats={selectedSeats}
+                    loaderFun={setShowLoaderFun}
+                    tripId={state.id}
+                />
 
-            <Grid container alignItems="center" mb={6} spacing={1.5}>
-                <Grid item xs={12} sm={9}>
-                    <FareDetails
-                        noOfSeats={selectedSeatsCount}
-                        farePerSeat={state.farePerSeat}
-                    />
+                <Grid container alignItems="center" mb={6} spacing={1.5}>
+                    <Grid item xs={12} sm={9}>
+                        <FareDetails
+                            noOfSeats={selectedSeatsCount}
+                            farePerSeat={state.farePerSeat}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={3} ml="auto">
+                        <StyledButton
+                            onClick={() => {
+                                handleFormSubmit();
+                            }}
+                            fullWidth
+                        >
+                            {t('checkout')}
+                        </StyledButton>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={3} ml="auto">
-                    <StyledButton
-                        onClick={() => {
-                            handleFormSubmit();
-                        }}
-                        fullWidth
-                    >
-                        {t('checkout')}
-                    </StyledButton>
-                </Grid>
-            </Grid>
-            <ConfirmDialog
-                title={t('confirmationTitle')}
-                open={showDialog}
-                handleClose={() => {
-                    setShowDialog(false);
-                }}
-                agreeText={t('agreeText')}
-                disagreeText={t('DisagreeText')}
-                handleAgreeFunction={() => {
-                    setShowLoader(true);
-                    formikRef.current?.handleSubmit();
-                }}
-            >
-                {t('dialogContent')}
-            </ConfirmDialog>
-            <FullScreenLoader open={showLoader} />
-        </>
+                <ConfirmDialog
+                    title={t('confirmationTitle')}
+                    open={showDialog}
+                    handleClose={() => {
+                        setShowDialog(false);
+                    }}
+                    agreeText={t('agreeText')}
+                    disagreeText={t('DisagreeText')}
+                    handleAgreeFunction={() => {
+                        setShowLoader(true);
+                        formikRef.current?.handleSubmit();
+                    }}
+                >
+                    {t('dialogContent')}
+                </ConfirmDialog>
+                <FullScreenLoader open={showLoader} />
+            </>
+        )
     );
 };
