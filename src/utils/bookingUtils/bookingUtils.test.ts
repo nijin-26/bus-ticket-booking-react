bbed439b8d6010ed7d02a5d188365a707ea4182c
RISHI J R <rishi@qburst.com>
2024-03-07 16:42:52 +0530

    fix: use describe to make individual test suites and added change logs

--- src/utils/bookingUtils/bookingUtils.test.ts
+++ src/utils/bookingUtils/bookingUtils.test.ts
@@ -1,45 +1,49 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { convertFormikDataToApiData, filterSelectedSeats } from '..';
 import { ISeatStatus } from '../../types';
 
-test('converts formik data to API payload', () => {
-    const input = {
-        passengers: [
+describe('Booking API converter function', () => {
+    it('converts formik data to API payload', () => {
+        const input = {
+            passengers: [
+                {
+                    seatNumber: 40,
+                    fullName: 'Rishi',
+                    age: '23',
+                    gender: 'male',
+                },
+            ],
+        };
+        const output = [
             {
                 seatNumber: 40,
-                fullName: 'Rishi',
-                age: '23',
-                gender: 'male',
+                passenger: {
+                    fullName: 'Rishi',
+                    age: 23,
+                    gender: 'male',
+                },
             },
-        ],
-    };
-    const output = [
-        {
-            seatNumber: 40,
-            passenger: {
-                fullName: 'Rishi',
-                age: 23,
-                gender: 'male',
-            },
-        },
-    ];
+        ];
 
-    expect(convertFormikDataToApiData(input)).toStrictEqual(output);
+        expect(convertFormikDataToApiData(input)).toStrictEqual(output);
+    });
 });
 
-test('filter selected seats', () => {
-    const input = [
-        {
-            seatNumber: 40,
-            status: ISeatStatus.BOOKED,
-        },
-        {
-            seatNumber: 41,
-            status: ISeatStatus.SELECTED,
-        },
-    ];
+describe('Seat filter function for booking', () => {
+    it('filter selected seats', () => {
+        const input = [
+            {
+                seatNumber: 40,
+                status: ISeatStatus.BOOKED,
+            },
+            {
+                seatNumber: 41,
+                status: ISeatStatus.SELECTED,
+            },
+        ];
 
-    const output = [41];
+        const output = [41];
 
-    expect(filterSelectedSeats(input)).toStrictEqual(output);
+        expect(filterSelectedSeats(input)).toStrictEqual(output);
+    });
 });
