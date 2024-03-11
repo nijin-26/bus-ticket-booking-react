a5f6b4ca9b05f49e00155ec7d3868e6507d0da8a
amaljoeqb <amal.joe@qburst.com>
2024-03-11 13:38:27 +0530

    add: test for getUserFromUserExternal

--- src/api/converters/user.converter.ts
+++ src/api/converters/user.converter.ts
@@ -2,13 +2,11 @@ import { IUser } from '../../types';
 import { IUserExternal } from '../types/user';
 
 export const getUserFromUserExternal = (userExternal: IUserExternal): IUser => {
-    console.log('getUserFromUserExternal input', userExternal);
     const user: IUser = {
         id: userExternal.id.toString(),
         fullName: userExternal.name,
         email: userExternal.email,
         phone: userExternal.phone,
     };
-    console.log('getUserFromUserExternal output', user);
     return user;
 };
