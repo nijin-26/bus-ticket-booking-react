ede6937c3445d564fe4262dd241aad37620d6e38
amaljoeqb <amal.joe@qburst.com>
2024-03-11 10:57:47 +0530

    add: test for getTripFromTripExternal

---
+++ src/api/converters/trip.converter.test.ts
@@ -0,0 +1,58 @@
+import { test, expect } from 'vitest';
+import { getTripFromTripExternal } from './trip.converter';
+import { IBusType, ISeatType, ITrip } from '../../types';
+import { ISeatTypeExternal, ITripExternal } from '../types/trip';
+
+test('getTripFromTripExternal should return the correct trip', () => {
+    const tripExternal: ITripExternal = {
+        id: 107,
+        tripDate: '2024-05-13T18:30:00.000Z',
+        departure: '2024-05-14T17:52:56.000Z',
+        arrival: '2024-05-14T21:52:56.000Z',
+        durationInHours: '3.5',
+        busId: 'N1',
+        busType: IBusType.AC,
+        seatType: ISeatTypeExternal.Seater,
+        totalSeats: 44,
+        farePerSeat: '1140.00',
+        origin: {
+            id: 8,
+            createdAt: '2024-02-13T03:20:13.404Z',
+            updatedAt: '2024-02-13T03:20:13.404Z',
+            name: 'Palakkad',
+            shortCode: 'PLK',
+        },
+        destination: {
+            id: 9,
+            createdAt: '2024-02-13T03:20:17.108Z',
+            updatedAt: '2024-02-13T03:20:17.108Z',
+            name: 'Pathanamthitta',
+            shortCode: 'PTA',
+        },
+    };
+
+    const expectedTrip: ITrip = {
+        id: '107',
+        origin: {
+            id: '8',
+            name: 'Palakkad',
+            shortCode: 'PLK',
+        },
+        destination: {
+            id: '9',
+            name: 'Pathanamthitta',
+            shortCode: 'PTA',
+        },
+        departureTimestamp: new Date('2024-05-14T17:52:56.000Z'),
+        arrivalTimestamp: new Date('2024-05-14T21:52:56.000Z'),
+        farePerSeat: 1140,
+        totalSeats: 46,
+        busType: IBusType.AC,
+        seatType: ISeatType.SEATER,
+        availableSeats: 44,
+    };
+
+    const trip = getTripFromTripExternal(tripExternal);
+
+    expect(trip).toEqual(expectedTrip);
+});
