1bbe47e3a60204f83d8dcfac694381601123ab04
Sania <you@example.com>
2024-03-14 12:04:20 +0530

    fix: default sort in sortGroup

--- src/components/actionBar/actionBar/ActionBar.tsx
+++ src/components/actionBar/actionBar/ActionBar.tsx
@@ -190,7 +190,6 @@ const ActionBar: React.FC<IActionBarProps> = ({ showFilterSort }) => {
                         <Autocomplete
                             fullWidth
                             options={filterLocationOptions(stopLocation)}
-                            // options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
                             value={startLocation}
                             onChange={handleStartSelect}
                             isOptionEqualToValue={(option, value) =>
--- src/components/actionBar/actionBarTab/ActionBarTab.tsx
+++ src/components/actionBar/actionBarTab/ActionBarTab.tsx
@@ -1,4 +1,4 @@
-import { Box, Tab } from '@mui/material';
+import { Box, Tab, useMediaQuery } from '@mui/material';
 import { TabContext, TabList } from '@mui/lab';
 import React, { useEffect, useState } from 'react';
 import ActionBar from '../actionBar/ActionBar';
@@ -6,6 +6,7 @@ import PnrSearch from '../pnrSearch/PnrSearch';
 import { Panel, WrapperPaper } from './ActionBarTab.styled';
 import { useTranslation } from 'react-i18next';
 import { useSearchParams } from 'react-router-dom';
+import { useTheme } from '@emotion/react';
 
 interface IActionBarProps {
     showFilterSort?: boolean;
@@ -18,6 +19,11 @@ const ActionBarTab: React.FC<IActionBarProps> = ({
     const [value, setValue] = useState('1');
     const [searchParams] = useSearchParams();
     const pnr = searchParams.get('pnr');
+
+    const { breakpointValues } = useTheme();
+    const isSmallScreen = useMediaQuery(
+        `(max-width:${breakpointValues.small})`
+    );
 
     const changeTabHandler = (_: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
@@ -33,7 +39,10 @@ const ActionBarTab: React.FC<IActionBarProps> = ({
         <WrapperPaper sx={{ px: 2 }} elevation={4}>
             <TabContext value={value}>
                 <Box>
-                    <TabList onChange={changeTabHandler}>
+                    <TabList
+                        onChange={changeTabHandler}
+                        variant={isSmallScreen ? 'fullWidth' : 'standard'}
+                    >
                         <Tab label={t('findBuses')} value="1"></Tab>
                         {!showFilterSort ? (
                             <Tab label={t('findTicket')} value="2"></Tab>
